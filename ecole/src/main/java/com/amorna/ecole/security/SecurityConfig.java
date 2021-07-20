a76b0b82bf69fbf72a6c541bde87b44a8aeaf3e7
Khaldoun <Khaldoun@192.168.1.11>
2021-07-20 14:38:03 +0000

    SECURITE FIN RESTE BCRIPT

--- ecole/src/main/java/com/amorna/ecole/security/SecurityConfig.java
+++ ecole/src/main/java/com/amorna/ecole/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.amorna.ecole.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,26 +13,39 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private DataSource dataSource;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder passwordEncoder=passwordEncoder();
 		System.out.println("******************************");
 		System.out.println(passwordEncoder.encode("1234"));
-		System.out.println(passwordEncoder.encode("1234"));
+		
 		System.out.println("******************************");
+		
+		/*
 		auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("1234")).roles("USER");
 		auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1234")).roles("USER");
 		auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN");
+		*/
+		auth.jdbcAuthentication()
+		    .dataSource(dataSource)
+		    .usersByUsernameQuery("select username as principal ,password as credentials,active from users where username =?")
+		    .authoritiesByUsernameQuery("select username as principal ,role as role from users_roles where username =?")
+		    .passwordEncoder(passwordEncoder)
+		    .rolePrefix("ROLE_");
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		//http.httpBasic();
-		http.formLogin();
+		http.formLogin().loginPage("/login");
 		
-		http.authorizeRequests().antMatchers("/save**/**","/delete**/**","/form**/**").hasRole("ADMIN");
+		http.authorizeRequests().antMatchers("/save**/**","/delete**/**","/form**/**,/admin/**").hasRole("ADMIN");
 		http.authorizeRequests().antMatchers("/etudiants**/**").hasRole("USER");
-		//http.authorizeRequests().anyRequest().authenticated();
+		
+		http.authorizeRequests().antMatchers("/user/**","/login","/webjars/**").permitAll();
+		http.authorizeRequests().anyRequest().authenticated();
 		//http.csrf().disable();
 		http.exceptionHandling().accessDeniedPage("/notAuthorized");
 	}
